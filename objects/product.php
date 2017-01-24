075231f885f0f0f6464d600032a87b0376e38e00
atlgl <atlgl>
2017-01-24 00:13:10 -0600

    commituno

---
+++ objects/product.php
@@ -0,0 +1,69 @@
+<?php
+class Product{
+    private $conn;
+    private $table_name="products";
+
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $created;
+
+    public function __construct($db){
+        $this->conn=$db;
+    }
+
+        // create product
+    function create(){
+
+        // query to insert record
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    name=:name, price=:price, description=:description, created=:created";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // posted values
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->price=htmlspecialchars(strip_tags($this->price));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+
+        // bind values
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":created", $this->created);
+
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }else{
+            echo "<pre>";
+                print_r($stmt->errorInfo());
+            echo "</pre>";
+
+            return false;
+        }
+    }
+
+    // read products
+    function readAll(){
+        // select all query
+        $query = "SELECT
+                    id, name, description, price, created
+                FROM
+                    " . $this->table_name . "
+                ORDER BY
+                    id DESC";
+        // prepare query statement
+        $stmt = $this->conn->prepare( $query );
+        // execute query
+        $stmt->execute();
+        return $stmt;
+    }
+
+}
+
+?>
---
+++ ventasmati.sql
@@ -0,0 +1,17 @@
+-- Es es el script de base de datos
+
+CREATE DATABASE IF NOT EXISTS ventasmati;
+USE ventasmati;
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(512) NOT NULL,
+  `description` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=64 ;
